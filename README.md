797502017ba18bb89d6158a6ba2b17c15a9d961f
TechFounder <techfounder@outlook.com>
2013-06-28 15:16:59 -0400

    Update files README.md and micro_blogger.rb

---
+++ README.md
@@ -0,0 +1,4 @@
+MicroBlogger
+============
+
+This a a micro bogging program using the GEM jumpstart_auth.  Written in Ruby, the program will allow you to tweet your messages to your Twitter account once you give access to the program.
--- micro_blogger.rb
+++ micro_blogger.rb
@@ -4,6 +4,34 @@ class MicroBlogger
 	attr_reader :client
 
 	def initialize
-		puts "initializing"
+		puts "initializing..."
+		@client = JumpstartAuth.twitter
 	end
-end		
+
+	def tweet(message)
+		@client.update(message)
+		#if message.length > 140
+	end
+
+	def run
+		puts "Welcome to the JSL Twitter CLient!"
+		command = ""
+		while command != "q"
+			printf "enter command: "
+			command = gets.chomp
+			case command
+				when 'q' then puts "Goodbye!"
+				when 't' then printf "What's your message? "
+					message = gets.chomp
+					tweet(message)
+					puts "OK, done!"
+					break
+				else
+					puts "Sorry, I dont; know to to #{command}"
+			end	
+		end		
+	end	
+end
+
+blogger = MicroBlogger.new
+blogger.run
